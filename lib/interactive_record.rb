7eff59625fce95cb627b3988169db313a2eb13bd
Brandon Teich <brandonteich123@gmail.com>
2019-02-15 00:01:56 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,37 +3,34 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
-  def self.table_name
+def self.table_name
     self.to_s.downcase.pluralize
   end
-  
+
   def self.column_names
     DB[:conn].results_as_hash = true
-    
-    sql = "pragma table_info('#{table_name}')"
-    
-    table_info = DB[:conn].execute(sql)
+
+    sql = "PRAGMA table_info('#{table_name}')"
+
+    table_name = DB[:conn].execute(sql)
     column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
+
+    table_name.each do |col|
+      column_names << col["name"]
     end
     column_names.compact
   end
-  
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
-  end
-  
+
   def table_name_for_insert
     self.class.table_name
   end
-  
+
   def col_names_for_insert
-    self.class.column_names.delete_if { |col| col == "id"}.join(", ")
+    self.class.column_names.delete_if {|column_name| column_name == "id"}.join(", ")
   end
-  
+
+  values = []
+
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
@@ -41,26 +38,35 @@ class InteractiveRecord
     end
     values.join(", ")
   end
-  
+
   def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    sql = <<-SQL
+    INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})
+    SQL
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
-  
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    sql = <<-SQL
+    SELECT * FROM #{self.table_name} WHERE name = '#{name}'
+    SQL
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(column_hash)
-    column_name = column_hash.keys[0]
-    column_value = column_hash.values[0]
-    if column_value.is_a? Integer
-      sql = "SELECT * FROM #{self.table_name} WHERE #{column_name}=#{column_value}"
-    elsif column_value.is_a? String
-      sql = "SELECT * FROM #{self.table_name} WHERE #{column_name}='#{column_value}'"
-    end
+  def self.find_by(attribute)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.to_a.flatten[0]} = #{attribute.to_a.flatten[0]} LIMIT 1"
     DB[:conn].execute(sql)
   end
   
